36c263cebf00867f1730f6b08f4762f463a722be
leonelEisinger <leoneleisinger@gmail.com>
2024-09-24 22:55:58 -0300

    more themes

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
     // Default theme
-    let isDarkTheme = false;
 
     $('.dropdown-item').click(function() {
         const selectedTheme = $(this).data('theme');
 
-        if (selectedTheme === 'dark' && !isDarkTheme) {
+        if (selectedTheme === 'dark') {
             $('#theme-stylesheet').attr('href', 'css/dark-theme.css');
 			$('#themeDropdown').text('Change theme (Dark)');
-            isDarkTheme = true;
-        } else if (selectedTheme === 'light' && isDarkTheme) {
+        } else if (selectedTheme === 'light') {
             $('#theme-stylesheet').attr('href', 'css/light-theme.css');
 			$('#themeDropdown').text('Change theme (Light)');
-            isDarkTheme = false;
+        } else if (selectedTheme === 'creative'){
+            $('#theme-stylesheet').attr('href', 'css/creative-theme.css');
+			$('#themeDropdown').text('Change theme (Creative)');
+        } else if (selectedTheme === 'vibrant'){
+            $('#theme-stylesheet').attr('href', 'css/vibrant-theme.css');
+            $('#themeDropdown').text('Change theme (Vibrant)');
         }
     });
 });
